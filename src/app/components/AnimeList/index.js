bbd4e0c1a91f74e96b315ebb705153f02c235aa9
Mrafisyahbani77 <mrafi.syahbani@gmail.com>
2024-07-29 08:50:49 +0700

    image

--- src/app/components/AnimeList/index.js
+++ src/app/components/AnimeList/index.js
@@ -1,7 +1,11 @@
+import Image from "next/image";
+
 const AnimeList = () => {
   return (
     <div className="grid grid-cols-3 gap-4 h-32">
-      <div className="bg-indigo-500">Kotak 1</div>
+      <div className="bg-indigo-500">
+        <Image src="https://placehold.co/600x400/png" width={600} height={600} alt="..."/>
+      </div>
       <div className="bg-indigo-500">Kotak 2</div>
       <div className="bg-indigo-500">Kotak 3</div>
       <div className="bg-indigo-500">Kotak 4</div>
