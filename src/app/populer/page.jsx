30fb5d56d81d3c3e48197a5fcbc2b61482c73e42
Mrafisyahbani77 <mrafi.syahbani@gmail.com>
2024-08-01 14:07:49 +0700

    list

--- src/app/populer/page.jsx
+++ src/app/populer/page.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+"use client";
+
+import AnimeList from "@/components/AnimeList";
+import HeaderMenu from "@/components/Utilities/HeaderMenu";
+import Pagination from "@/components/Utilities/Pagination";
+import { useEffect, useState } from "react";
 
 const page = () => {
-  return (
-    <div className="mt-40 text-color-accent">Populer Page</div>
-  )
-}
+  const [page, setPage] = useState(1);
+  const [topAnime, setTopAnime] = useState([]);
 
-export default page
+  const fetchData = async () => {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime?page=${page}`
+    );
+    const data = await response.json();
+    setTopAnime(data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [page]);
+
+  return (
+    <>
+      <HeaderMenu />
+      <AnimeList api={topAnime} />
+      <Pagination />
+    </>
+  );
+};
+
+export default page;
