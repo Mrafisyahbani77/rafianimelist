080f515e134fd49d1f66cacdcd143f5ef0de5bd0
Mrafisyahbani77 <mrafi.syahbani@gmail.com>
2024-08-02 09:14:09 +0700

    sesi5

--- src/app/search/[keyword]/page.jsx
+++ src/app/search/[keyword]/page.jsx
@@ -1,13 +1,12 @@
+import { getAnimeResponse } from "@/app/libs/api-libs";
 import AnimeList from "@/components/AnimeList";
 import Header from "@/components/AnimeList/Header";
+
 
 const Page = async ({ params }) => {
   const { keyword } = params;
   const decodedKeyword = decodeURIComponent(keyword);
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_BASE_URL}/anime?q=${keyword}`
-  );
-  const searchAnime = await response.json();
+  const searchAnime = await getAnimeResponse("anime", `q=${decodedKeyword}`)
 
   return (
     <>
